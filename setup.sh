5133c408dbd65501a2e17263d4299beed4f18c68
theo <lesgooo1993@proton.me>
2023-06-23 13:06:06 +0530

    added setup.sh; lil edit in package.json

---
+++ setup.sh
@@ -0,0 +1,8 @@
+echo "Removing .git directory..." && rm -rf .git
+
+while [[ -z $proj_name ]]
+	do read -rp "Enter Project name: " proj_name
+done
+
+sed -i "s/sveltekit-starter-template/$proj_name/" package.json \
+	&& echo "Renaming complete."
