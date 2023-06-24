cf4e1073e138100eacd238828779fc2cd993ecdf
theo <lesgooo1993@proton.me>
2023-06-24 15:20:43 +0530

    setup.sh: initialize git repository, create git alias to git push, npm run build & rsync to vkdbois server

--- setup.sh
+++ setup.sh
@@ -7,3 +7,12 @@ done
 
 sed -i "s/sveltekit-starter-template/$proj_name/" package.json \
 	&& echo "Renaming complete."
+
+while [[ -z $dir ]]
+	do read -rp "Enter directory to sync to: " dir
+done
+
+echo "Initializing Git repository..." && git init && echo "Initialized."
+echo "Creating git alias..." && \
+git config alias.p "!git push && npm run build && rsync -avrz --delete build/ root@vkdbois.xyz:/var/www/$dir" && \
+echo -e "Git alias created.\nExecute 'git p' to push, build and rsync all at once."
